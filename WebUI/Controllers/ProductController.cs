96e846f9203a632d16d3a5cf32c7535b975122a7
LAPTOP-DDUTAMI\SEO <ghost41m@gmail.com>
2023-03-21 13:56:47 +0900

    first

---
+++ WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET: Product
+        public ActionResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
---
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using Moq;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            this.kernel = kernel;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.GetService(serviceType); 
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product { Name = "Football", Price = 25 },
+                new Product { Name = "Surf board", Price = 179  },
+                new Product { Name = "Running shoes", Price = 95 }
+            });
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+    }
+}
