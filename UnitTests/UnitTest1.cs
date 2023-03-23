822284f9455ff06d84bbff5437a00825cdee3164
Hyojoong Seo <ghost41m@hotmail.com>
2023-03-23 14:21:53 +0900

    test unit

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Domain.Abstract;
+using Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WebUI.Controllers;
 
 namespace UnitTests
 {
@@ -7,8 +14,27 @@ namespace UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginte()
         {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]{
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+                new Product { ProductID = 4, Name = "P4" },
+                new Product { ProductID = 5, Name = "P5" },
+                new Product { ProductID = 6, Name = "P6" }
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 3);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P4");
         }
     }
 }
--- WebUI/Controllers/ProductController.cs
+++ WebUI/Controllers/ProductController.cs
@@ -10,6 +10,7 @@ namespace WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repository)
         {
@@ -17,9 +18,12 @@ namespace WebUI.Controllers
         }
 
         // GET: Product
-        public ActionResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products
+                .OrderBy(p=>p.ProductID)
+                .Skip((page-1) * PageSize)
+                .Take(PageSize));
         }
     }
 }
