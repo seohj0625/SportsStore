e7fd955054ed73f0a9ebc8ac49782de3bfd95861
Hyojoong Seo <ghost41m@hotmail.com>
2023-03-23 15:34:05 +0900

    7장 완료

--- WebUI/Controllers/ProductController.cs
+++ WebUI/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebUI.Models;
 
 namespace WebUI.Controllers
 {
@@ -20,10 +21,21 @@ namespace WebUI.Controllers
         // GET: Product
         public ViewResult List(int page = 1)
         {
-            return View(repository.Products
-                .OrderBy(p=>p.ProductID)
-                .Skip((page-1) * PageSize)
-                .Take(PageSize));
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                            .OrderBy(p => p.ProductID)
+                            .Skip((page - 1) * PageSize)
+                            .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+
+            return View(model);
         }
     }
 }
